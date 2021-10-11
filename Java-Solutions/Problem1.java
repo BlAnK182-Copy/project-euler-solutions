7230992a7066247d532b2ff2dd764c2056eff79a
Abhinav Kumar <abhinav.30.kr@gmail.com>
2021-10-11 23:09:01 +0530

    added Problem1.java
    added Java solution to Problem 1 on Project Euler

---
+++ Java-Solutions/Problem1.java
@@ -0,0 +1,15 @@
+public class Problem1 {
+    public static void main(String[] args) {
+        final int NUMBER = 1000;
+
+        long sum = 0;
+        
+        for (int i = 2; i<NUMBER; i++) {
+            if (i%3==0 || i%5==0) {
+                sum+=i;
+            }
+        }
+
+        System.out.println("The sum is: " + sum);
+    }
+}
