7c4f57a749dfcea24d394a110229a9afb23131f2
Abhinav Kumar <abhinav.30.kr@gmail.com>
2021-10-12 19:49:53 +0530

    modified Solutions 1 to 5 - Python

--- Python-Solutions/Problem1.py
+++ Python-Solutions/Problem1.py
@@ -1,4 +1,4 @@
-#Problem 1
+#Problem 1 - Multiples of 3 or 5
 
 NUMBER = 1000
 
--- Python-Solutions/Problem3.py
+++ Python-Solutions/Problem3.py
@@ -1,5 +1,8 @@
-#Slow Method 
+#Problem 3 - Largest Prime Factor
 
+#Old Method - Very Slow
+
+'''
 def isPrime(number):
     for i in range(2, number//2+1):
         if number%i==0:
@@ -7,8 +10,7 @@ def isPrime(number):
     return True 
 
 
-NUMBER = 600851475143
-
+NUMBER = 3453450
 
 largest = 0
 for i in range(2, NUMBER):
@@ -17,3 +19,28 @@ for i in range(2, NUMBER):
             if largest <i:
                 largest = i
                 print(f"Current largest: {i}")
+
+'''
+
+#New method - fast
+
+#Theory: for a number n = p1 * p2 * p3...* p(n-1) where pi (from 1 to n-1) is prime (but not necessarily unique) for pi > 1
+#if we continuously divide n with it's smallest factor, then the number that we divided it by after which n becomes 1, is the largest prime factor
+
+
+def smallestPrimeFactor(number):
+    assert number>1
+    for i in range(2, int(number*0.5) + 1):
+        if number%i == 0:
+            return i
+    return number #the number itself is the smallest prime factor of the number
+
+NUMBER = 600851475143
+largestPrimeFactor = 1
+
+while NUMBER!=1:
+    factor = smallestPrimeFactor(NUMBER)
+    NUMBER //= factor 
+    largestPrimeFactor = factor 
+
+print(f"Largest Prime factor of {NUMBER} is: {largestPrimeFactor}")
