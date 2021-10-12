ec0f7e2cebe1a4089ade0ba87404d81a581609d1
Abhinav Kumar <abhinav.30.kr@gmail.com>
2021-10-12 18:01:12 +0530

    added Python Solution to Problem 4

---
+++ Python-Solutions/Problem4.py
@@ -0,0 +1,16 @@
+#Problem 4
+
+def palindromeCheck(number):
+    if str(number)[::-1].lower() == str(number).lower():
+        return True
+    return False 
+
+
+largestPalindrome = 0
+for i in range(100, 999):
+    for j in range(100, 999):
+        if palindromeCheck(i*j) == True:
+            if largestPalindrome<i*j:
+                largestPalindrome = i*j
+
+print(f"Largest palindrome product made by 2 three-digit numbers is: {largestPalindrome}")
